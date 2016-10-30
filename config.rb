4f2df8663a8cfe8278fcdfce6bc18434103c9876
Naokazu Terada <naokazu.terada@gmail.com>
2016-10-30 17:40:48 +0900

    Use external pipeline

--- config.rb
+++ config.rb
@@ -16,6 +16,14 @@ set :haml, { :attr_wrapper => "\"" }
 # URL access xxx.hmtl -> /xxx/
 activate :directory_indexes
 
+activate :external_pipeline,
+   name: :webpack,
+   command: build? ?
+   "./node_modules/webpack/bin/webpack.js --bail -p" :
+   "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
+   source: ".tmp/dist",
+   latency: 1
+
 # Per-page layout changes:
 #
 # With no layout
