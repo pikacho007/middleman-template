c3e389e86de473bf137904a3f4cd92f4729f499d
Naokazu Terada <naokazu.terada@gmail.com>
2017-09-07 12:51:46 +0900

    - Move script tag position - Detect Edge

--- source/script/main.js
+++ source/script/main.js
@@ -1,33 +1,13 @@
 var ua = navigator.userAgent;
-document.isIE = ua.match(/msie/i);
-document.isIE6 = ua.match(/msie [6.]/i);
-document.isIE7 = ua.match(/msie [7.]/i);
-document.isIE8 = ua.match(/msie [8.]/i);
-document.isIE9 = ua.match(/msie [9.]/i);
-document.isIE10 = ua.match(/msie [10.]/i);
-document.isIE10 = ua.match(/msie [11.]/i);
-
-if(document.isIE){
-  document.body.className += 'ie';
-  if(document.isIE6){
-    document.body.className += 'ie6';
-  }
-  else if(document.isIE7) {
-    document.body.className += 'ie7';
-  }
-  else if(document.isIE8) {
-    document.body.className += 'ie8';
-  }
-  else if(document.isIE9) {
-    document.body.className += 'ie9';
-  }
-  else if(document.isIE10) {
-    document.body.className += 'ie10';
-  }
-  else if(document.isIE11) {
-    document.body.className += 'ie11';
-  }
+if( ua.match(/(msie|MSIE)/) || ua.match(/(T|t)rident/) ) {
+  var ieVersion = parseInt(ua.match(/((msie|MSIE)\s|rv:)([\d\.]+)/)[3]);
+  document.body.className += ' ie'+ieVersion;
 }
+else if( ua.match(/Edge/) ) {
+  document.body.className += ' edge';
+}
+console.log(ua);
+
 document.isMobile = false;
 document.isTablet = false;
 if(
