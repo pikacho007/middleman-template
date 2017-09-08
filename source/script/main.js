324b8dcbc327e71334a3ea2d59b4b0010acf81b7
Naokazu Terada <naokazu.terada@gmail.com>
2017-09-08 11:12:03 +0900

    Change conditions of device decisions

--- source/script/main.js
+++ source/script/main.js
@@ -1,26 +1,26 @@
 var ua = navigator.userAgent;
-if( ua.match(/(msie|MSIE)/) || ua.match(/(T|t)rident/) ) {
+if(ua.match(/(msie|MSIE)/) || ua.match(/(T|t)rident/)) {
   var ieVersion = parseInt(ua.match(/((msie|MSIE)\s|rv:)([\d\.]+)/)[3]);
   document.body.className += ' ie'+ieVersion;
 }
-else if( ua.match(/Edge/) ) {
+else if(ua.match(/Edge/i)) {
   document.body.className += ' edge';
 }
 
 document.isMobile = false;
 document.isTablet = false;
 if(
-  0 < ua.indexOf('iPhone') ||
-  0 < ua.indexOf('iPod') ||
-  (0 < ua.indexOf('Android') && 0 < ua.indexOf('Mobile')) ||
-  0 < ua.indexOf('iPad') ||
-  0 < ua.indexOf('Android')
+  ua.match(/iPhone/i) ||
+  ua.match(/iPod/i) ||
+  (ua.match(/Android/i) && ua.match(/Mobile/i)) ||
+  ua.match(/iPad/i) ||
+  ua.match(/Android/i)
 ) {
   document.isMobile = true;
 }
 if(
-  0 < ua.indexOf('iPad') ||
-  (ua.indexOf('Mobile') == 0 && 0 < ua.indexOf('Android'))
+  ua.match(/iPad/i) ||
+  (ua.match(/Mobile/i) && ua.match(/Android/i))
 ) {
   document.isTablet = true;
   var $meta = $('head>meta[name="viewport"]');
