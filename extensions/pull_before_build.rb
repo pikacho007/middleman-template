8a89eb2a01d12d16d20e6bdb443e40b3b8635068
Naokazu Terada <naokazu.terada@gmail.com>
2018-02-03 17:45:08 +0900

    ビルド前にpullはできるようになったが、下記問題が発生
    deployを最初にした際に、まだbuildがないのでpullしないのだが、その時にforce_pushされてしまう

---
+++ extensions/pull_before_build.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+# To avoid force push build directory and
+class PullBeforeBuild < ::Middleman::Extension
+  def before_build(_builder)
+    p ':::before_build:::'
+    if Dir.exist?('build')
+      Dir.chdir 'build'
+      p 'Execute "g pull origin"'
+      system 'g pull origin'
+    else
+      p 'There isn\'t build dir yet, so do nothing.'
+    end
+  end
+end
+
+::Middleman::Extensions.register(:pull_before_build, PullBeforeBuild)
